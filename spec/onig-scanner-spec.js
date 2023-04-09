ee52dee4e655f74b96ceab3ddd3f780d700e8d36
Andrew Dupont <github@andrewdupont.net>
2023-04-08 21:22:51 -0700

    (I suppose these patterns are strings)

--- spec/onig-scanner-spec.js
+++ spec/onig-scanner-spec.js
@@ -8,15 +8,15 @@ describe("OnigScanner", () => {
   beforeEach(async () => await onig.ready)
 
   it("caches patterns so that equivalent pattern(s) return the same instance", () => {
-    let firstScanner = new onig.oniguruma.OnigScanner([/lorem/])
+    let firstScanner = new onig.oniguruma.OnigScanner(["lorem"])
     for (let i = 0; i < 10; i++) {
-      let scanner = new onig.oniguruma.OnigScanner([/lorem/])
+      let scanner = new onig.oniguruma.OnigScanner(["lorem"])
       expect(scanner === firstScanner).to.be.eql(true)
     }
 
-    let anotherScanner = new onig.oniguruma.OnigScanner([/ipsum/, /dolor/, /lorem/])
+    let anotherScanner = new onig.oniguruma.OnigScanner(["ipsum", "dolor", "lorem"])
     for (let i = 0; i < 10; i++) {
-      let scanner = new onig.oniguruma.OnigScanner([/ipsum/, /dolor/, /lorem/])
+      let scanner = new onig.oniguruma.OnigScanner(["ipsum", "dolor", "lorem"])
       expect(scanner === anotherScanner).to.be.eql(true)
     }
   })
